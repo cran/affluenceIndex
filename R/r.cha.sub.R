d3a96d66311278e646500106cb5b8f55ffbb8540
Alicja Wolny-Dominiak <alicja.wolny-dominiak@ue.katowice.pl>
2022-01-05 14:50:08 +0000

    version 2.1

--- R/r.cha.sub.R
+++ R/r.cha.sub.R
@@ -1,5 +1,4 @@
-r.cha.sub <-
-function(x.sub, x, weight.sub, weight, k, beta){
+r.cha.sub <- function(x.sub, x, weight.sub, weight, k, beta){
   n <- length(x)
   if(is.null(weight)){
     weight <- rep(1, n)
@@ -7,6 +6,6 @@ function(x.sub, x, weight.sub, weight, k, beta){
   }
   rhow <- k*weighted.median(x, weight)
   ind <- ifelse(x.sub > rhow, 1, 0)
-  r.cha <- sum((1-(rhow/(x.sub))^beta*ind)*weight.sub)/sum(weight.sub)
+  r.cha <- sum(((1-(rhow/(x.sub[x.sub>0]))^beta)*ind[x.sub>0])*weight.sub[x.sub>0])/sum(weight.sub)
   return(r.cha)
 }
--- R/r.fgt.sub.R
+++ R/r.fgt.sub.R
@@ -1,5 +1,4 @@
-r.fgt.sub <-
-function(x.sub, x, weight.sub, weight, k, alpha){
+r.fgt.sub <- function(x.sub, x, weight.sub, weight, k, alpha){
   n <- length(x)
   if(is.null(weight)){
     weight <- rep(1, n)
@@ -7,6 +6,6 @@ function(x.sub, x, weight.sub, weight, k, alpha){
   }
   rhow <- k*weighted.median(x, weight)
   ind <- ifelse(x.sub > rhow, 1, 0)
-  r.fgt <- sum(((((x.sub - rhow)/x.sub)*ind)*weight.sub)^alpha)/sum(weight.sub)
+  r.fgt <- sum(((((x.sub - rhow)/rhow)*ind)^alpha*weight.sub))/sum(weight.sub)
   return(r.fgt)
 }
